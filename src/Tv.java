519a481a466403933b19db34598f0313cf8cca03
hilaalyildiz <75807952+hilaalyildiz@users.noreply.github.com>
2022-07-29 18:47:55 +0300

    Add TV on/off and volume up/down methods

--- src/Tv.java
+++ src/Tv.java
@@ -6,16 +6,69 @@ public class Tv {
     private String marka;
     private ArrayList<Kanal> kanallar;
 
+    private boolean acik;
+
+    private int ses;
     public Tv(String marka,String boyut){
         this.marka = marka;
         this.boyut = boyut;
+        ses =10;
 
+        kanallar = new ArrayList<>();
         kanalOlustur();
+        this.acik = false;
+
+    }
+
+    public void sesArttir(){
+        if(ses<=20 && acik == true){
+            ses++;
+            System.out.println("Ses seviyesi : "+ses);
+        }else{
+            System.out.println("Ses en yüksek seviyede");
+        }
+    }
+
+    public void sesAzalt(){
+        if(ses>0 && acik){
+            ses--;
+            System.out.println("Ses seviyesi : "+ses);
+        }else{
+            System.out.println("Ses en düşük seviyede");
+        }
+    }
+
+    public void tvAc(){
+        if (acik==false){
+            acik= true;
+            System.out.println("Tv acildi");
+        }else{
+            System.out.println("Tv zaten acik");
+        }
+    }
+
+    public void tvKapat(){
+        if (acik==true){
+            acik= false;
+            System.out.println("Tv kapatildi");
+        }else{
+            System.out.println("Tv zaten kapali");
+        }
     }
 
     private void kanalOlustur() {
         HaberKanali cnn = new HaberKanali("CNN",1,"Genel Haber");
         kanallar.add(cnn);
+
+        HaberKanali bein = new HaberKanali("BeinSpor",3,"Spor Haberi");
+        kanallar.add(bein);
+
+        MuzikKanali dreamTurk = new MuzikKanali("Dream Turk",2,"Yerli");
+        kanallar.add(dreamTurk);
+
+        MuzikKanali numberOne = new MuzikKanali("Number One",4,"Yabanci");
+        kanallar.add(numberOne);
+
     }
 
     public String getMarka() {
--- src/Tv_Simulation.java
+++ src/Tv_Simulation.java
@@ -17,7 +17,21 @@ public class Tv_Simulation {
                 case 1:
                     tvKurveKanallariOlustur();
                     break;
-                
+
+                case 2:
+                    tvAc();
+                    break;
+                case 3:
+                    sesArttir();
+                    break;
+
+                case 4:
+                    sesAzalt();
+                    break;
+
+                case 7:
+                    tvKapat();
+                    break;
                 case 8:
                     menuGoster();
                     break;
@@ -30,6 +44,38 @@ public class Tv_Simulation {
                     break;
 
             }
+        }
+    }
+
+    private static void sesArttir() {
+        if(tv !=null){
+            tv.sesArttir();
+        } else{
+            System.out.println("Once tv'yi kurun ve kanallari olusuturun");
+        }
+    }
+
+    private static void sesAzalt() {
+        if(tv !=null){
+            tv.sesAzalt();
+        } else{
+            System.out.println("Once tv'yi kurun ve kanallari olusuturun");
+        }
+    }
+
+    private static void tvKapat() {
+        if(tv!=null){
+            tv.tvKapat();
+        }else {
+            System.out.println("Once tv'yi kurun ve kanallari olusuturun");
+        }
+    }
+
+    private static void tvAc(){
+        if(tv!=null){
+            tv.tvAc();
+        }else {
+            System.out.println("Once tv'yi kurun ve kanallari olusuturun");
         }
     }
 
@@ -56,8 +102,8 @@ public class Tv_Simulation {
                 "0 -- Cıkıs\n" +
                 "1 -- Televizyonu Kur \n"+
                 "2 -- Televizyonu Ac \n"+
-                "3 -- Sesi Ac \n"+
-                "4 -- Sesi Kapat \n"+
+                "3 -- Sesi Arttır \n"+
+                "4 -- Sesi Azalt \n"+
                 "5 -- Kanal Degistir \n"+
                 "6 -- Kanal Bilgisi Goster \n"+
                 "7 -- Televizyonu Kapat \n"+
